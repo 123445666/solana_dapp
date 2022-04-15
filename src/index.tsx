c53eb38aa5601f6d674b332262041d4348ee28be
funaway89 <funaway89@gmail.com>
2022-04-15 20:09:15 +0200

    first dapp connect to phantom

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { WalletKitProvider } from "@gokiprotocol/walletkit";
+
+import App from "./App";
+
+import "./index.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <WalletKitProvider
+      defaultNetwork="devnet"
+      app={{
+        name: "My App",
+      }}
+    >
+      <App />
+    </WalletKitProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
